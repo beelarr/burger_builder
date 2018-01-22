7c297d0cd4130f324215c6d56fd857f2037b28c0
beelarr <bryonl@me.com>
2018-01-22 11:14:58 -0600

    Ingredient to class and add proptypes validation

--- src/components/Burger/Ingredients/Ingredients.js
+++ src/components/Burger/Ingredients/Ingredients.js
@@ -1,40 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Ingredients.css';
+import PropTypes from 'prop-types';
 
-const ingredient = props => {
-    let singleIngredient = null;
+class Ingredient extends Component {
+    render () {
+        let singleIngredient = null;
 
-    switch (prop.type) {
-        case('bread-bottom'):
-            singleIngredient = <div className={styles.BreadBottom} />;
-            break;
-        case('bread-top'):
-            singleIngredient = (
-                <div className={styles.BreadTop}>
-                    <div className={styles.Seeds1} />
-                    <div className={styles.Seeds2} />
-                </div>
-            );
-            break;
-        case('meat'):
-            singleIngredient = <div className={styles.Meat} />;
-            break;
-        case('cheese'):
-            singleIngredient = <div className={styles.Cheese} />;
-            break;
-        case('bacon'):
-            singleIngredient = <div className={styles.Bacon} />;
-            break;
-        case('lettuce'):
-            singleIngredient = <div className={styles.Salad} />;
-            break;
-        default:
-            singleIngredient = null;
+        switch (this.prop.type) {
+            case('bread-bottom'):
+                singleIngredient = <div className={styles.BreadBottom}/>;
+                break;
+            case('bread-top'):
+                singleIngredient = (
+                    <div className={styles.BreadTop}>
+                        <div className={styles.Seeds1}/>
+                        <div className={styles.Seeds2}/>
+                    </div>
+                );
+                break;
+            case('meat'):
+                singleIngredient = <div className={styles.Meat}/>;
+                break;
+            case('cheese'):
+                singleIngredient = <div className={styles.Cheese}/>;
+                break;
+            case('bacon'):
+                singleIngredient = <div className={styles.Bacon}/>;
+                break;
+            case('lettuce'):
+                singleIngredient = <div className={styles.Salad}/>;
+                break;
+            default:
+                singleIngredient = null;
+        }
+
+        return singleIngredient;
     }
+};
 
-    return singleIngredient;
-
+Ingredient.propTypes = {
+    type: PropTypes.string.isRequired
 };
 
 
-export default ingredient;
+export default Ingredient;
