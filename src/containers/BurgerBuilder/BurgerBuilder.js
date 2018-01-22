e4eaabf457bfa1c4e21d8a90e84b880b535daa60
beelarr <bryonl@me.com>
2018-01-22 14:58:30 -0600

    Add logic to keep negative totals from errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,6 +39,7 @@ class BurgerBuilder extends Component {
 
     removeIngredientHandler = type => {
         const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0){return;}
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
