984121682e08740e15701eec8475eed2bef0cf48
beelarr <bryonl@me.com>
2018-01-22 13:56:42 -0600

    Add dynamic message if no ingredients exists

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import Ingredient from './Ingredients/Ingredients';
 import styles from './Burger.css';
 
 const burger = props => {
-    const transformedIngredients = Object.keys(props.ingredients).map(ingredient => {
+    let transformedIngredients = Object.keys(props.ingredients).map(ingredient => {
         return [...Array(props.ingredients[ingredient])].map((_, index) => {
             return <Ingredient key={ingredient + index} type={ingredient} /> ;
         })
-    });
+    }).reduce((arr, element) => arr.concat(element), []);
+    // arr is the empty arr defined as the second arg, element is each iteration
+
+    transformedIngredients.length === 0 ? transformedIngredients = <p>Are you on a gluten only diet?!?!</p> : transformedIngredients;
+
     return(
         <div className={styles.Burger}>
             <Ingredient type="bread-top"/>
