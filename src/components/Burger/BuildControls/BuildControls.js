f8e23b3ae684e63579e7823c94a5a9fd1c2e9c1b
beelarr <bryonl@me.com>
2018-01-23 14:26:09 -0600

    Add css to build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,7 @@ const buildControls = props => (
                 disabled={props.disabled[ctrl.type]}
 
             />))}
+            <button className={styles.OrderButton}>ORDER NOW</button>
     </div>
 );
 
