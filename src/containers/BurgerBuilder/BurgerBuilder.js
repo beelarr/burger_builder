b0d3108e2ccee525750d228b51eede6eb0764515
beelarr <bryonl@me.com>
2018-01-23 15:15:58 -0600

    Basic modal construction

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     lettuce: .5,
@@ -71,7 +73,10 @@ class BurgerBuilder extends Component {
             //sets this version of state from a number to true or false for disabling buttons
         }
         return (
-            <Aux style>
+            <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger
                     ingredients={this.state.ingredients}
                 />
