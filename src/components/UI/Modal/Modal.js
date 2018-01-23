19ac21b1ace70b4607163018296b342192a51678
beelarr <bryonl@me.com>
2018-01-23 14:52:08 -0600

    Add modal and css for it

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import styles from './Modal.css';
+
+const modal = props => (
+    <div className={styles.Modal} >
+        {props.children}
+    </div>
+);
+
+export default modal;
