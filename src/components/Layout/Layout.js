117379048e8df47f4a8e8a8b2cea125e09de974a
beelarr <bryonl@me.com>
2018-01-25 10:39:12 -0600

    Add open close logic for side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
@@ -8,17 +7,32 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-      <div>   Backdrop</div>
-      <main
-          className={classes.Content}
-      >
-          { props.children }
-          </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawerOpenClosed = () => {
+        this.setState({showSideDrawer: false})
+    };
+
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerOpenClosed}/>
+                <div>   Backdrop</div>
+                <main
+                    className={classes.Content}
+                >
+                    { this.props.children }
+                </main>
+            </Aux>
+        )
+    }
+}
+
+
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,19 +4,28 @@ import styles from './SideDrawer.css';
 
 import Logo from '../../Logo/Logo';
 import NavComponents from '../NavComponents/NavComponents';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = props => {
 
-    return(
-        <div className={styles.SideDrawer}>
-            <div className={styles.Logo}>
-                <Logo />
-            </div>
-            <nav>
-                <NavComponents/>
-            </nav>
+    let classArray = [styles.SideDrawer, styles.Closed];
+    if (props.open) {
+        classArray = classArray.filter(el => el !== styles.Closed).concat(styles.Open)
+    };
 
-        </div>
+    return(
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={classArray.join(' ')} >
+                <div className={styles.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavComponents/>
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
