3ced64f0a40ecfec3c51b0aa09c3dbd419f3b5e5
beelarr <bryonl@me.com>
2018-01-29 16:04:10 -0600

    Add get for orders, and error handling

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    };
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then(res => {
+            const orders = [];
+            for (let key in res.data){
+                orders.push({
+                    ...res.data[key],
+                    id: key
+                })
+            }
+            this.setState({
+                orders: orders,
+                loading: false
+            })
+                .catch(error => {
+                    alert(`Something is wrong probably ${error.message}`);
+                    this.setState({loading: false})
+                })
+        })
+
+    };
+
     render() {
         return (
             <div>
@@ -12,4 +40,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
