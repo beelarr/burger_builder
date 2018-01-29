c12c973562e53468254390af7075701437cf2f7e
beelarr <bryonl@me.com>
2018-01-29 15:34:13 -0600

    Add order and orders components with styling and routing

---
+++ src/components/Order/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Order.css';
+
+const order = props => (
+
+    <div className={styles.Order}>
+        <p>Ingredients: Lettuce (1)</p>
+        <p>Price: <strong>$4.50</strong></p>
+    </div>
+);
+
+
+export default order
