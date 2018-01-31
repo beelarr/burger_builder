79d9fc3536e781bd58aac258979f5e5adc958d6d
beelarr <bryonl@me.com>
2018-01-31 13:52:18 -0600

    Inital build of reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as actionTypes from './actions';
+
+
+const initalState = {
+    ingredients: {},
+    totalPrice: 1
+};
+
+const reducer = (state = initalState, action) => {
+
+}
+
+export default reducer;
